845063f41023829400a704f9bf864e90530807e9
MajidJowDev <majid.jow@gmail.com>
2022-08-31 23:44:13 +0430

    Initial commit

---
+++ src/main/java/mjz/springframework/springrestclienteg/SpringRestClientEgApplication.java
@@ -0,0 +1,13 @@
+package mjz.springframework.springrestclienteg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestClientEgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestClientEgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mjz/springframework/springrestclienteg/SpringRestClientEgApplicationTests.java
@@ -0,0 +1,13 @@
+package mjz.springframework.springrestclienteg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestClientEgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
