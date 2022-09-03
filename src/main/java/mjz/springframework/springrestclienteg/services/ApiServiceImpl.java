9edb506085560a0aed473a27d447cb42c444f4bc
MajidJowDev <majid.jow@gmail.com>
2022-09-04 00:53:46 +0430

    23- 9. URI Components Builder... added api url in application.properties and injected it in service class with Spring

--- src/main/java/mjz/springframework/springrestclienteg/services/ApiServiceImpl.java
+++ src/main/java/mjz/springframework/springrestclienteg/services/ApiServiceImpl.java
@@ -2,8 +2,10 @@ package mjz.springframework.springrestclienteg.services;
 
 import mjz.springframework.api.domain.User;
 import mjz.springframework.api.domain.UserData;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -12,9 +14,12 @@ public class ApiServiceImpl implements ApiService {
 
     private RestTemplate restTemplate;
 
+    private final String api_url;
+
     //because we have a constructor, Spring will automatically autowires a RestTemplate for us
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url) {
         this.restTemplate = restTemplate;
+        this.api_url = api_url;
     }
 
     @Override
@@ -26,7 +31,15 @@ public class ApiServiceImpl implements ApiService {
         // does a get request to the uri and bind the result to the class we want
         // the result returned by API (JSON Object) will bind to the properties of our domain classes based on the name of the properties
         // and Jackson will do the bindings automatically
-        UserData userData = restTemplate.getForObject("https://dummyjson.com/users?limit=" + limit, UserData.class);
+
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                .fromUriString(api_url)
+                .queryParam("limit", limit);
+
+        UserData userData = restTemplate.getForObject(uriBuilder.toUriString(), UserData.class);
+
+
+       // UserData userData = restTemplate.getForObject("https://dummyjson.com/users?limit=" + limit, UserData.class);
         return userData.getUsers();
     }
 }
